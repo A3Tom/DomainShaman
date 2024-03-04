ba1d24439f1ca2ffebadb9ae85f7d8cdb7d9bc15
A3Tom <Tom_Esq@Yahoo.com>
2024-03-04 14:33:16 +0000

    Refactored to add in error handling and reduce noise in actual script

--- version_chanter.ps1
+++ version_chanter.ps1
@@ -1,50 +1,96 @@
-git fetch --tags
-$tags = git tag
-#$mostRecentCommit = git log -1
-$targetCommitId = "e967c221e439ffe4dfcf0c7d985dd012fe48c0af"
-$mostRecentCommit = git log $targetCommitId -1
 $pipelineProvider = "github"
-$minorVersionMergeSources = @("bugfix", "slampy")
+$minorVersionMergeSources = @("feature")
+
+
+function Assert-IsGithubMergeCommit {
+    param (
+        [string]$targetCommitMessage
+    )
+
+    Write-Host "Target Commit Message: $targetCommitMessage"
+
+    If ($targetCommitMessage -Match 'Merge: [a-z0-9]{7}\s[a-z0-9]{7}') {
+        Write-Host "This is a merge commit. Sound tae proceed."
+        return $true
+    }
+    
+    Write-Error "This is not a merge commit. Skipping versioning." -Category InvalidArgument
+    exit 1
+}
+
+function Assert-IsMergeCommit {
+    param (
+        [string]$targetCommitMessage
+    )
+
+    Write-Host "Pipeline provider: $pipelineProvider"
+
+    if ($pipelineProvider -eq "github") {
+        Assert-IsGithubMergeCommit -targetCommitMessage $targetCommitMessage
+    } elseif ($pipelineProvider -eq "azure") {
+        Write-Error "Here mate, let me get GH Actions sorted first"
+        exit 1
+    } else {
+        Write-Error "Unsupported pipeline provider: $pipelineProvider"
+        exit 1
+    }
+}
+
+function Assert-IsArgumentValidCommitId {
+    param (
+        [string]$commitId
+    )
+
+    if ($null -eq $commitId) {
+        Write-Error "Please provide a commit id as the first argument" -Category InvalidArgument
+        exit 1
+    }
+    
+    $targetCommitMessage = git log $commitId -1
+    Assert-IsMergeCommit -targetCommitMessage $targetCommitMessage
+
+    $true
+}
+
+function Set-TargetCommitId {
+    param (
+        [string]$commitId
+    )
+    if (Assert-IsArgumentValidCommitId -commitId $commitId){
+        $targetCommitId = $commitId
+    }
+    $targetCommitId
+}
 
 function Get-MergeSourceBranchRegex {
     $branchNames = $minorVersionMergeSources -join "|"
     "[from\w]\S+[\/](?:$branchNames)\/\S+"
 }
 
-function Assert-IncrementMinorVersion {
-    $minorVersionMergeSources = Get-MergeSourceBranchRegex
-    If ($mostRecentCommit -match $minorVersionMergeSources) {
+function Assert-IsMinorVersionIncrement {
+    $minorVersionMergeSourceRegex = Get-MergeSourceBranchRegex
+    $minorVersionBranchNames = $minorVersionMergeSources -join ", "
+    If ($targetCommitMessage -match $minorVersionMergeSourceRegex) {
+        Write-Host "[$minorVersionBranchNames] match found in commit message. Increasin the minor version."
         return $true
     }
+
+    Write-Host "[$minorVersionBranchNames] match not found in commit message. Increasin the patch version."
     $false
 }
 
-function Assert-IsGithubMergeCommit {
-    Write-Host "Checking if this is a merge commit $mostRecentCommit"
-    If ($mostRecentCommit -match 'Merge: [a-z0-9]{7}\s[a-z0-9]{7}') {
-        Write-Host "$_"
-        $true
-    }
-    $false
-}
 
-function Get-IsMergeCommit {
-    Write-Host "Pipeline provider: $pipelineProvider"
-
-    if ($pipelineProvider -eq "github") {
-        Assert-IsGithubMergeCommit
-    } elseif ($pipelineProvider -eq "azure") {
-        $false
-    } else {
-        Write-Host "Unsupported pipeline provider: $pipelineProvider"
-    }
+function Get-CurrentReleaseVersions {
+    git fetch --tags
+    $tags = git tag
+    $tags | Where-Object { $_ -like 'release/v*' } | ForEach-Object { $_ -replace "release/v", "" }
 }
 
 function Get-CurrentVersion {
     param (
         [object[]]$tags
     )
-    $versionTags = $tags | Where-Object { $_ -like 'release/v*' } | ForEach-Object { $_ -replace "release/v", "" }
+    $versionTags = Get-CurrentReleaseVersions
     $versions = $versionTags | ForEach-Object { [System.Version]::new($_) } | Sort-Object 
 
     $currentVersion = $versions[-1]
@@ -64,34 +110,27 @@ function Get-NextVersion {
     Write-Host "Next minor version: $nextMinorVersion"
     Write-Host "Next patch version: $nextPatchVersion"
 
-    if ($isMinorRevision) {
-        $nextMinorVersion
-    } else {
-        $nextPatchVersion
-    }
+    $nextVersion = if ($isMinorRevision) { $nextMinorVersion } else { $nextPatchVersion }
+    Write-Host "Next version: $nextVersion"
+
+    $nextVersion
+}
+
+function Set-NextVersionTag {
+    param (
+        [System.Version]$nextVersion,
+        [string]$targetCommitId
+    )
+
+    Write-Host "Settin phasers tae malky"
+    git tag -a "release/v$nextVersion" $targetCommitId -m "Release v$nextVersion"
+    git push --tag
 }
 
 
-$isMergeVersion = Get-IsMergeCommit 
-
-if (!$isMergeVersion)
-{
-    Write-Host "This is not a merge commit. Skipping versioning."
-    exit 0
-}
-Write-Host "This is a merge commit. Proceeding with versioning."
-
-$isMinorRevision = Assert-IncrementMinorVersion
-Write-Host "Is minor revision: $isMinorRevision"
-
+$commitId = Set-TargetCommitId -commitId $args[0]
+$isMinorRevision = Assert-IsMinorVersionIncrement
 $latestVersion = Get-CurrentVersion -tags $tags
 $nextVersion = Get-NextVersion -latestVersion $latestVersion -isMinorRevision $isMinorRevision
 
-Write-Host "Next version: $nextVersion"
-
-return $nextVersion
-
-# # Tag git commit
-# git tag -a "release/v$nextVersion" $targetCommitId -m "Release v$nextVersion"
-# git push --tag
-
+Set-NextVersionTag -nextVersion $nextVersion -targetCommitId $commitId
